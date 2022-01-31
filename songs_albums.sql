57c7142a9c7a2d4519ff19c780e4796d306cd4fd
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-01-30 23:01:36 -0800

    completed

---
+++ songs_albums.sql
@@ -0,0 +1,69 @@
+/*
+ * TODO: Create a table called 'songs' that has the following fields:
+ * - id - integer, primary key
+ * - name - string
+ * - album_id - foreign key
+ * 
+ * Note that album - song is a one-to-many relationship, so no bridge table is needed.
+ */
+--  Code here
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    song_name VARCHAR(70) NOT NULL,
+    album_id INTEGER NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES Albums(id)
+);
+CREATE TABLE Albums (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(70) NOT NULL,
+    artist VARCHAR(70) NOT NULL,
+    year_published INTEGER NOT NULL
+);
+/* 
+ * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
+ */
+INSERT INTO Songs (song_name, album_id)
+VALUES ('Bored to Death', 1),
+    ('Born in the U.S.A', 2),
+    ('Come Together', 3),
+    ('Hotel California', 4);
+INSERT INTO Albums (name, artist, year_published)
+VALUES ('The Dark Side of the Moon', 'Pink Floyd', 1973),
+    ('Abbey Road', 'The Beatles', 1969),
+    ('Hotel California', 'Eagles', 1976),
+    ('Born in the U.S.A.', 'Bruce Springsteen', 1984),
+    ('California', 'Blink-182', 2016);
+/* Required to get result in column format */
+.headers on.mode column
+/* Queries */
+SELECT *
+FROM Songs;
+SELECT *
+FROM Albums;
+/* 
+ * TODO: Write a table join query to construct a table of Song Name : Album Name
+ */
+SELECT *
+FROM Songs
+    JOIN Albums ON Songs.album_id = Albums.id;
+/*
+ * TODO: Find all albums published between 1970 and 1980.
+ */
+SELECT *
+FROM Albums
+WHERE year_published BETWEEN 1970 AND 1980;
+/*
+ * TODO: Find all songs on albums published between 1970 and 1980. 
+ *(Hint: Use a table join.)
+ */
+SELECT Song_name
+FROM Songs
+    JOIN Albums ON Songs.album_id = Albums.id
+WHERE Albums.year_published BETWEEN 1970 AND 1980;
+/*
+ * TODO: Find all songs on albums with names containing 'California'.
+ */
+SELECT Song_name
+FROM Songs
+    JOIN Albums ON Songs.album_id = Albums.id
+WHERE Albums.name IN ('California')
